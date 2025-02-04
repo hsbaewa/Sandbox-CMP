807199d73f7212462cc921cdc3561c79604a737c
hsbaewa <hsbaewa@8snippet.com>
2025-02-05 08:55:30 +0900

    #13 MaterialTheme 커스터마이징.

---
+++ composeApp/src/commonMain/kotlin/kr/co/hs/sandbox/cmp/ui/Color.kt
@@ -0,0 +1,14 @@
+package kr.co.hs.sandbox.cmp.ui
+
+import androidx.compose.ui.graphics.Color
+
+val md_color_blue_gray_050 = Color(0xFFECEFF1)
+val md_color_blue_gray_100 = Color(0xFFCFD8DC)
+val md_color_blue_gray_200 = Color(0xFFB0BEC5)
+val md_color_blue_gray_300 = Color(0xFF90A4AE)
+val md_color_blue_gray_400 = Color(0xFF78909C)
+val md_color_blue_gray_500 = Color(0xFF607D8B)
+val md_color_blue_gray_600 = Color(0xFF546E7A)
+val md_color_blue_gray_700 = Color(0xFF455A64)
+val md_color_blue_gray_800 = Color(0xFF37474F)
+val md_color_blue_gray_900 = Color(0xFF263238)
---
+++ composeApp/src/commonMain/kotlin/kr/co/hs/sandbox/cmp/ui/Theme.kt
@@ -0,0 +1,131 @@
+package kr.co.hs.sandbox.cmp.ui
+
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalRippleConfiguration
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RippleConfiguration
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = lightColorScheme,
+        content = {
+            CompositionLocalProvider(
+                LocalRippleConfiguration provides RippleConfiguration(
+                    MaterialTheme.colorScheme.surfaceBright,
+                    RippleAlpha(
+                        .5f,
+                        .3f,
+                        .3f,
+                        .5f
+                    )
+                )
+            ) {
+                content()
+            }
+        }
+    )
+}
+
+
+private val lightColorScheme = lightColorScheme(
+    /**
+     * <primary>
+     *     Button BG
+     */
+    primary = md_color_blue_gray_900,
+
+    /**
+     * <surfaceContainerHighest>
+     *     스위치 off 배경
+     */
+    surfaceContainerHighest = md_color_blue_gray_100,
+
+    /**
+     * <outline>
+     *     스위치 off 테두리
+     */
+    outline = md_color_blue_gray_800,
+
+    /**
+     * <onPrimary>
+     *     Button font Tint
+     */
+    onPrimary = md_color_blue_gray_100,
+
+    /**
+     * <inversePrimary>
+     *     SecFileDialogTextButton disabled content color
+     */
+    inversePrimary = md_color_blue_gray_400,
+
+    /**
+     * <onSurface>
+     *     TopBar font
+     *     TopBar navigation icon tint
+     *     Button disabled BG(with alpha)
+     *     Button disabled font tint(with alpha)
+     */
+    onSurface = md_color_blue_gray_100,
+
+    /**
+     * <surface>
+     *     TopBar BG
+     */
+    surface = md_color_blue_gray_900,
+
+    /**
+     * <background>
+     *     default BG
+     */
+    background = md_color_blue_gray_050,
+
+    /**
+     * <surfaceContainer>
+     *     NavigationBar BG
+     *     SelectableItem selected BG (with alpha)
+     */
+    surfaceContainer = md_color_blue_gray_100,
+
+    /**
+     * <onSecondaryContainer>
+     *     NavigationBarItem selected icon Tint
+     */
+    onSecondaryContainer = md_color_blue_gray_500,
+
+    /**
+     * <secondaryContainer>
+     *     NavigationBarItem selected icon BG
+     *     NavigationBarItem selected font
+     */
+    secondaryContainer = md_color_blue_gray_100,
+
+    /**
+     * <onSurfaceVariant>
+     *     TopBar action button tint
+     *     NavigationBarItem contents
+     */
+    onSurfaceVariant = md_color_blue_gray_700,
+
+    /**
+     * <secondary>
+     *     DropdownMenu BG
+     */
+    secondary = md_color_blue_gray_200,
+
+    /**
+     * <onSecondary>
+     *     DropdownMenuItem font
+     */
+    onSecondary = md_color_blue_gray_900,
+
+    /**
+     * ripple effect
+     */
+    surfaceBright = md_color_blue_gray_300
+)
