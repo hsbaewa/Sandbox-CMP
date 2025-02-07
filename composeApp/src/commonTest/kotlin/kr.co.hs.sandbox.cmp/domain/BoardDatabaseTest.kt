06f12e5d1f28e63ec49a537befc664376c6db9ec
hsbaewa <hsbaewa@8snippet.com>
2025-02-07 17:27:01 +0900

    #32 Database 테스트 코드 추가.

--- composeApp/src/commonTest/kotlin/kr.co.hs.sandbox.cmp/domain/BoardDatabaseTest.kt
+++ composeApp/src/commonTest/kotlin/kr.co.hs.sandbox.cmp/domain/BoardDatabaseTest.kt
@@ -5,37 +5,48 @@ import androidx.compose.ui.test.runComposeUiTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
 import kr.co.hs.domain.usecase.NoErrorUseCase
 import kr.co.hs.sandbox.cmp.UsingContext
-import kr.co.hs.sandbox.domain.usecase.GetButtonClickCountUseCase
-import kr.co.hs.sandbox.domain.usecase.UpCountButtonClickUseCase
+import kr.co.hs.sandbox.domain.entity.BoardEntity
+import kr.co.hs.sandbox.domain.usecase.CreateBoardUseCase
+import kr.co.hs.sandbox.domain.usecase.GetBoardUseCase
 import org.koin.core.context.loadKoinModules
 import org.koin.test.KoinTest
 import org.koin.test.inject
+import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-class ButtonClickCountTest : KoinTest, UsingContext() {
+class BoardDatabaseTest : KoinTest, UsingContext() {
 
-    private val upCount: UpCountButtonClickUseCase by inject()
-    private val getCount: GetButtonClickCountUseCase by inject()
+    private val createBoard: CreateBoardUseCase by inject()
+    private val getBoard: GetBoardUseCase by inject()
 
-    @OptIn(ExperimentalTestApi::class, ExperimentalCoroutinesApi::class)
+    @OptIn(ExperimentalCoroutinesApi::class, ExperimentalTestApi::class)
     @Test
-    fun do_test_up_count_usecase() = runComposeUiTest {
-        loadKoinModules(kr.co.hs.sandbox.data.preference.di.repositoryModule)
+    fun do_test_create_board_usecase() = runComposeUiTest {
+        loadKoinModules(kr.co.hs.sandbox.data.database.di.repositoryModule)
 
         runTest {
 
+            val title = "title_${Random.nextInt()}"
+            val contents = "contents_${Random.nextInt()}"
+
             coroutineScope {
-                flow { emit(getCount().first()) }
+
+                createBoard(
+                    object : BoardEntity {
+                        override val title: String = title
+                        override val contents: String = contents
+                    }
+                )
                     .map {
                         when (it) {
                             is NoErrorUseCase.Result.Exception -> throw it.t
@@ -43,22 +54,9 @@ class ButtonClickCountTest : KoinTest, UsingContext() {
                         }
                     }
                     .onEach {
-                        assertEquals(0, it)
+                        assertEquals(title, it.title)
                     }
-                    .catch { throw it }
-                    .launchIn(this)
-            }
-
-            coroutineScope {
-                upCount()
-                    .map {
-                        when (it) {
-                            is NoErrorUseCase.Result.Exception -> throw it.t
-                            is NoErrorUseCase.Result.Success -> it.data
-                        }
-                    }
-                    .onEach { if (!it) throw Exception("failed up count") }
-                    .flatMapConcat { flow { emit(getCount().first()) } }
+                    .flatMapConcat { getBoard(title) }
                     .map {
                         when (it) {
                             is NoErrorUseCase.Result.Exception -> throw it.t
@@ -66,12 +64,26 @@ class ButtonClickCountTest : KoinTest, UsingContext() {
                         }
                     }
                     .onEach {
-                        assertEquals(1, it)
+                        assertEquals(title, it.title)
                     }
                     .catch { throw it }
                     .launchIn(this)
             }
 
+            coroutineScope {
+
+                val list = getBoard()
+                    .map {
+                        when (it) {
+                            is NoErrorUseCase.Result.Exception -> throw it.t
+                            is NoErrorUseCase.Result.Success -> it.data
+                        }
+                    }
+                    .toList()
+
+                assertTrue(list.isNotEmpty())
+            }
         }
     }
+
 }
